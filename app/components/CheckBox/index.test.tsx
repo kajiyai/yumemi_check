dcbfb3b4f997b100ad1d9667172c44ed883e983d
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-12-09 22:38:18 +0900

    チェックボックスコンポーネントのテストを追加

---
+++ app/components/CheckBox/index.test.tsx
@@ -0,0 +1,14 @@
+import { render, fireEvent } from '@testing-library/react';
+import CheckBox from './index';
+
+test('renders CheckBox and handles click', () => {
+  const handleChange = jest.fn();
+  const { getByLabelText } = render(
+    <CheckBox id="test" value={1} label="Test" handleCheckboxChange={handleChange} />
+  );
+
+  const checkbox = getByLabelText('Test');
+  fireEvent.click(checkbox);
+
+  expect(handleChange).toHaveBeenCalled();
+});
